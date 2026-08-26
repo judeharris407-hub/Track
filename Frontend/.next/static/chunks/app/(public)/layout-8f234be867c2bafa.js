0a1865d81ce3590072340531b3cd74672b12bba1
Jude <judeharris407@gmail.com>
2026-08-26 18:27:15 +0100

    Fix Windows backslash directory paths in Next.js app folder

--- Frontend/.next/static/chunks/app/(public)/layout-8f234be867c2bafa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[288],{6243:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,11))}},function(e){e.O(0,[42,680,11,971,117,744],function(){return e(e.s=6243)}),_N_E=e.O()}]);
